ef1e890f77d1eb461507c7e5efd5cce4df985d86
Jure Varlec <jure.varlec@gmail.com>
2010-09-25 18:35:49 +0200

    posix: port tests over to dash, with exceptions.
    Testing framework is updated to allow for more than two shells. Each test
    can specify which shells it works with. Currently, the pipesleeps and
    manysleep tests don't work in dash because it lacks proper stdin reading.

--- tests/test_header.sh
+++ tests/test_header.sh
@@ -1,12 +1,13 @@
 export LC_ALL=C
-source ../prll.sh
+. ../prll.sh
 PATH="../:$PATH"
+test_shell="$(ps -p $$ -o comm=)"
 
 echo
-echo $TEST_DESCRIPTION
-if [[ -n $ZSH_VERSION ]] ; then
-   echo "Shell: zsh"
-else
-   echo "Shell: bash"
+echo "$TEST_DESCRIPTION"
+echo "Shell: $test_shell"
+if [ -n "$SUPPORTED_SHELLS" ] ; then
+    echo "$SUPPORTED_SHELLS" | grep "$test_shell" > /dev/null || \
+    { echo "Shell not supported in this test."; exit 0; }
 fi
 echo
